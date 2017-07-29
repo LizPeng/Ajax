d7a189cdcd5e79a67c1b45205050dd129fb7fa98
LizPeng <lizhipeng2013@163.com>
2017-07-29 13:04:14 +0800

    1

--- 22.js
+++ 22.js
@@ -44,3 +44,65 @@ function resizeDiv(){
 window.onresize = function(){
   throttle(resizeDiv)
 }
+
+//22.4 自定义事件
+function inheritPrototype(subType, superType){
+  var prototype = Object(superType.prototype);
+  prototype.constructor = subType;
+  subType.prototype = prototype;
+}
+function EventTarget() {
+  this.handlers = {};
+}
+
+EventTarget.prototype = {
+  constructor:EventTarget,
+  addHandler: function(type, handler) {
+    if(typeof this.handlers[type] == 'undefined'){
+      this.handlers[type] = [];
+    }
+    this.handlers[type].push(handler);
+  },
+  fire: function(event) {
+    if(!event.target){
+      event.target = this;
+    }
+    if(this.handlers[event.type] instanceof Array){
+      var handlers = this.handlers[event.type];
+      for(var i= 0, len = handlers.length; i < len ;i ++){
+        handlers[i](event);
+      }
+    }
+  },
+  removeHandler: function(type, handler) {
+    if(this.handlers[type] instanceof Array) {
+      var handlers = this.handlers[type];
+      for(var i = 0, len = handlers.length; i<len; i++){
+        if(handlers[i] === handler){
+          break;
+        }
+      }
+      handlers.splice(i, 1);
+    }
+  }
+};
+
+function Person(name, age) {
+  EventTarget.call(this)
+  this.name= name;
+  this.age = age;
+}
+//使用寄生组合继承EventTarget.
+inheritPrototype(Person, EventTarget);
+Person.prototype.say = function(message) {
+  this.fire({type:'message', message:message});
+};
+//调用方式
+function handleMessage(event) {
+  alert(event.target.name + " says: "+ event.message);
+}
+//创建新的person
+var person = new Person("Nicholas", 28);
+//添加一个事件处理从程序
+person.addHandler("message", handleMessage);
+person.say("Hi there.");
